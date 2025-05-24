1e5e20e947de213f1008d38106d4e2f5fccc011a
junjuny0227 <junjuny0227@gmail.com>
2025-05-24 23:10:46 +0900

    포멧팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
-import "@/styles/globals.css";
+import type { Metadata } from 'next';
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
-  title: "pokedocs",
-  description: "pokedocs",
+  title: 'pokedocs',
+  description: 'pokedocs',
 };
 
 const RootLayout = ({
