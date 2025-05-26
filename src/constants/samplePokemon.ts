6f11c61f8eb24bde84124a7c7d3f5b5c163b1be2
junjuny0227 <junjuny0227@gmail.com>
2025-05-26 11:58:10 +0900

    샘플 데이터 생성

---
+++ src/constants/samplePokemon.ts
@@ -0,0 +1,58 @@
+export const samplePokemon = [
+  {
+    id: 1,
+    name: 'bulbasaur',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
+    types: ['grass', 'poison'],
+  },
+  {
+    id: 4,
+    name: 'charmander',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
+    types: ['fire'],
+  },
+  {
+    id: 7,
+    name: 'squirtle',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
+    types: ['water'],
+  },
+  {
+    id: 25,
+    name: 'pikachu',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+    types: ['electric'],
+  },
+  {
+    id: 39,
+    name: 'jigglypuff',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/39.png',
+    types: ['normal', 'fairy'],
+  },
+  {
+    id: 94,
+    name: 'gengar',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/94.png',
+    types: ['ghost', 'poison'],
+  },
+  {
+    id: 130,
+    name: 'gyarados',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/130.png',
+    types: ['water', 'flying'],
+  },
+  {
+    id: 150,
+    name: 'mewtwo',
+    image:
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png',
+    types: ['psychic'],
+  },
+] as const;
