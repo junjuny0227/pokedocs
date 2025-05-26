792fc23b7482292d6a52cc68f59905a52e753bff
junjuny0227 <junjuny0227@gmail.com>
2025-05-26 22:43:17 +0900

    main 페이지 임시 완성

--- src/pageContainer/MainPage/index.tsx
+++ src/pageContainer/MainPage/index.tsx
@@ -1,5 +1,38 @@
+import { Footer, Header, PokemonCard } from '@/components';
+import { samplePokemon } from '@/constants';
+import { cn } from '@/libs/style';
+
 const MainPage = () => {
-  return <div></div>;
+  return (
+    <div
+      className={cn(
+        'min-h-screen',
+        'px-4',
+        'py-12',
+        'bg-gradient-to-br',
+        'from-indigo-900',
+        'via-purple-900',
+        'to-pink-800',
+      )}
+    >
+      <Header />
+      <div
+        className={cn(
+          'grid',
+          'grid-cols-1',
+          'gap-8',
+          'sm:grid-cols-2',
+          'md:grid-cols-3',
+          'lg:grid-cols-4',
+        )}
+      >
+        {samplePokemon.map(pokemon => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default MainPage;
